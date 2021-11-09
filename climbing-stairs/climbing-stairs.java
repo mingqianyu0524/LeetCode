cb5b3a49f0e5ba6ea77ff6867b52e095e05b03ac
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2021-11-09 10:45:11 +0800

    Time: 0 ms (100.00%), Space: 35.7 MB (80.26%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    // solution 1 dp
+    public int climbStairs(int n) {
+        int[] dp = new int[n];
+        for (int i=0;i<n;i++) {
+            if (i==0) dp[i] = 1;
+            else if (i==1) dp[i] = 2;  
+            else dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n-1];
+    }
+}
