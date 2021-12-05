b34749f1ee0911ccc37c2f43dd17ac599df9265c
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2021-12-05 21:34:40 +0800

    Time: 0 ms (100.00%), Space: 35.4 MB (99.66%) - LeetHub

---
+++ unique-paths/unique-paths.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] T = new int[m+1][n+1];
+        for (int i=1; i<=m; ++i) {
+            for (int j=1; j<=n; ++j) {
+                if (i==1 && j==1) T[i][j] = 1;
+                else T[i][j] = T[i-1][j] + T[i][j-1];
+            }
+        }
+        return T[m][n];
+    }
+}
