c3e95757788fd77d05af3fcb95397b7cc49a6bb8
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-01-09 22:29:09 +0800

    Time: 30 ms (15.87%), Space: 54.3 MB (8.24%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,12 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
         Map<String, List> ans = new HashMap<>();
+        int[] counter = new int[26];
         for (String s : strs) {
-            char[] ca = s.toCharArray(); 
-            Arrays.sort(ca);
-            // ca.toString() doesn't work, why?
-            String key = String.valueOf(ca);
+            Arrays.fill(counter, 0);
+            for (char c : s.toCharArray()) counter[c-'a']++;
+            
+            // int arr -> sb -> str, somehow int arr doesn't work, find out why!
+            StringBuilder sb = new StringBuilder("");
+            for (int i=0; i<26; i++) {
+                sb.append('#');
+                sb.append(counter[i]);
+            }
+            String key = sb.toString();
+            
             if (!ans.containsKey(key)) ans.put(key, new ArrayList());
             ans.get(key).add(s);
         }
