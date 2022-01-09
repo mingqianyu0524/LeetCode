18a3f25a6fec66130ac9d63ce53e9171d9de9d0d
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-01-09 20:06:21 +0800

    Time: 3 ms (33.33%), Space: 104.7 MB (32.44%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,8 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        // max_profit: max_profit from day[1, ..., i];
+        // min_price: min_price from day[1, ..., i];
+        // Target: Find max_profit on the n_th day, 1D DP -> updating vars.
         int n = prices.length;
         int min_price = prices[0], max_profit = 0;
         
