7907d7d45386ca5aa5865fb1fd2314f6631d3c2f
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-01 21:17:13 +0800

    Time: 0 ms (100.00%), Space: 41.2 MB (73.58%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,22 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        dp[0] = nums[0];
+        int prev = nums[0];
         if (n == 1) return nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int cur = Math.max(nums[0], nums[1]);
         // steal the first house
         for (int i = 2; i < n-1; i++) {
-            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+            int tmp = prev;
+            prev = cur;
+            cur = Math.max(tmp + nums[i], cur);
         }
-        int res = dp[n-2];
-        // reset the array
-        Arrays.fill(dp, 0);
-        dp[1] = nums[1];
+        int res = cur;
+        // reset
+        prev = 0;
+        cur = nums[1];
         // steal the last house
         for (int i = 2; i < n; i++) {
-            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+            int tmp = prev;
+            prev = cur;
+            cur = Math.max(tmp + nums[i], cur);
         }
-        return Math.max(res, dp[n-1]);
+        return Math.max(res, cur);
     }
 }
