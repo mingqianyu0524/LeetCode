8bddb329667d608bc20a4358647a896221ed642d
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-02 23:51:19 +0800

    Time: 1 ms (78.48%), Space: 41.7 MB (64.41%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -10,31 +10,29 @@ class MyQueue {
     }
     
     public void push(int x) {
-        if (s1.empty()) {
+        if (s1.empty())
             front = x;
-        }
-        while (!s1.isEmpty()) {
-            s2.push(s1.pop());
-        }
-        s2.push(x);
-        while (!s2.isEmpty()) {
-            s1.push(s2.pop());
-        }
+        s1.push(x);
     }
     
     public int pop() {
-        int res = s1.pop();
-        if (!s1.empty())
-            front = s1.peek();
-        return res;
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
     }
     
     public int peek() {
+        if (!s2.isEmpty()) {
+            return s2.peek();
+        }
         return front;
     }
     
     public boolean empty() {
-        return s1.isEmpty();
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
 
