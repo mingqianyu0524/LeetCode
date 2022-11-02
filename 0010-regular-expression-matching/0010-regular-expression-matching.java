aef84887fd71499076fe0dec674802d7b4baf0f6
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-02 19:16:56 +0800

    Time: 1 ms (100.00%), Space: 42.5 MB (72.67%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,19 +1,31 @@
 class Solution {
+    enum Result {TRUE, FALSE}
+    private Result[][] cache;
     public boolean isMatch(String s, String p) {
+        cache = new Result[s.length()+1][p.length()+1];
         return dfs(s, p, 0, 0);
     }
     private boolean dfs(String s, String p, int i, int j) {
+        if (cache[i][j] != null) {
+            return cache[i][j] == Result.TRUE;
+        }
         if (i >= s.length() && j >= p.length()) {
             return true;
         }
         if (j >= p.length()) {
             return false;
         }
+        boolean ans;
         boolean match = i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
         if (j + 1 < p.length() && p.charAt(j+1) == '*') {
-            return dfs(s, p, i, j+2) || (match && dfs(s, p, i+1, j));
+            ans = dfs(s, p, i, j+2) || (match && dfs(s, p, i+1, j));
         }
-        if (match) return dfs(s, p, i+1, j+1);
-        return false;
+        else if (match) {
+            ans = dfs(s, p, i+1, j+1);
+        } else {
+            ans = false;
+        }
+        cache[i][j] = ans ? Result.TRUE : Result.FALSE;
+        return ans;
     }
 }
