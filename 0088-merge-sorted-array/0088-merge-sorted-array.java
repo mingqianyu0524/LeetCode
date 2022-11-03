fda5e68541f3fe7f27368168457fff1257190736
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-03 14:58:23 +0800

    Time: 1 ms (55.12%), Space: 42.5 MB (71.59%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,15 +1,12 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] copy = new int[m];
-        for (int i = 0; i < m; i++) {
-            copy[i] = nums1[i];
-        }
-        int p1 = 0, p2 = 0;
-        for (int p = 0; p < m+n; p++) {
-            if (p2 >= n || p1 < m && copy[p1] < nums2[p2]) {
-                nums1[p] = copy[p1++];
+        int p1 = m - 1, p2 = n - 1;
+        for (int p = m + n - 1; p >= 0; p--) {
+            if (p2 < 0) break;
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1--];
             } else {
-                nums1[p] = nums2[p2++];
+                nums1[p] = nums2[p2--];
             }
         }
     }
