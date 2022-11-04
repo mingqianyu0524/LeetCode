df07d38c7bf416e7c5ab40f9e1f1e7d415333e94
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-04 14:58:43 +0800

    Time: 1 ms (99.74%), Space: 43.1 MB (88.60%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,19 +1,18 @@
 class Solution {
     public int trap(int[] height) {
-        int ans = 0, cur = 0;
-        Stack<Integer> stack = new Stack<>();
-        while (cur < height.length) {
-            if (stack.isEmpty() || height[cur] <= height[stack.peek()]) {
-                stack.push(cur++);
-            } else {
-                int bot = stack.pop();
-                if (!stack.isEmpty()) {
-                    int top = stack.peek();
-                    int h = Math.min(height[cur], height[top]) - height[bot];
-                    int d = cur - top - 1;
-                    ans += (h * d);
-                }
-            }
+        int n = height.length, ans = 0;
+        if (n == 0) return 0;
+        int[] left_max = new int[n], right_max = new int[n];
+        left_max[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            left_max[i] = Math.max(height[i], left_max[i-1]);
+        }
+        right_max[n-1] = height[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            right_max[i] = Math.max(height[i], right_max[i+1]);
+        }
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(left_max[i], right_max[i]) - height[i];
         }
         return ans;
     }
