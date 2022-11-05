dd426dd944ac044721299460f68379c0833b4a09
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-05 20:58:23 +0800

    Time: 2 ms (84.45%), Space: 43.5 MB (86.68%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -2,15 +2,19 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> answer = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                answer.add("FizzBuzz");
-            } else if (i % 3 == 0) {
-                answer.add("Fizz");
-            } else if (i % 5 == 0) {
-                answer.add("Buzz");
-            } else {
-                answer.add(Integer.toString(i));
+            String s = "";
+            boolean divisibleBy3 = (i % 3 == 0);
+            boolean divisibleBy5 = (i % 5 == 0);
+            if (divisibleBy3) {
+                s += "Fizz";
             }
+            if (divisibleBy5) {
+                s += "Buzz";
+            } 
+            if (s.equals("")) {
+                s += Integer.toString(i);
+            }
+            answer.add(s);
         }
         return answer;
     }
