ba2dedd5597380264303fe574923596c58fee9c1
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-05 16:17:46 +0800

    Time: 5 ms (30.19%), Space: 42.1 MB (48.52%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,25 +1,24 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         StringBuilder res = new StringBuilder();
-        Set<Character> set = new HashSet<>();
-        
         // find the length of the shortest str
         int len = Integer.MAX_VALUE;
         for (String s : strs) {
             len = Math.min(len, s.length());
         }
         int i = 0;
+        Character cur = new Character('A');
         while (i < len) {
             for (String s : strs) {
-                if (set.isEmpty()) {
-                    set.add(s.charAt(i));
+                if (cur.equals('A')) {
+                    cur = s.charAt(i);
                 }
-                if (!set.contains(s.charAt(i))) {
+                if (!cur.equals(s.charAt(i))) {
                     return res.toString();
                 }
             }
-            res.append(set.iterator().next());
-            set.clear();
+            res.append(cur);
+            cur = new Character('A');
             i++;
         }
         return res.toString();
