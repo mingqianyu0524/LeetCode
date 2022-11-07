be633ac13b67d448ec063df94ba8152986c7202e
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-07 10:42:30 +0800

    Time: 2 ms (90.59%), Space: 40.2 MB (93.89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -9,7 +9,7 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == '}' || c == ']' || c == ')') {
+            if (map.containsKey(c)) {
                 if (stack.isEmpty() || stack.pop() != map.get(c)) return false;
             }
             else {
