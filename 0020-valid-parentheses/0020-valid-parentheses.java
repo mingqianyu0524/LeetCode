9c5c3106a0dde7c0714e07a6d3c468749a68c28d
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-07 10:41:27 +0800

    Time: 5 ms (31.12%), Space: 41.9 MB (56.73%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 == 1) return false;
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+        
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '}' || c == ']' || c == ')') {
+                if (stack.isEmpty() || stack.pop() != map.get(c)) return false;
+            }
+            else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
