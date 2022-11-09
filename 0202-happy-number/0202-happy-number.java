7abaf307cd65945b4814daca84d869a72e909064
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-09 11:31:33 +0800

    Time: 4 ms (27.39%), Space: 42 MB (7.70%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,20 +1,19 @@
 class Solution {
+    private int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int m = n % 10;
+            sum += m*m;
+            n = n / 10;
+        }
+        return sum;
+    }
     public boolean isHappy(int n) {
         Set<Integer> set = new HashSet<>();
-        while (n != 1) {
-            int sum = 0;
-            while (n > 0) {
-                int m = n % 10;
-                sum += m*m;
-                n = n / 10;
-            }
-            if (!set.contains(sum)) {
-                set.add(sum);
-                n = sum;
-            } else {
-                return false;
-            }
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = getNext(n);
         }
-        return true;
+        return n == 1;
     }
 }
