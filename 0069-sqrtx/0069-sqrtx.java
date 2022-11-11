f827f222c57bbae9f59db8afd419fc8487ea5dcc
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-11 18:45:10 +0800

    Time: 1 ms (100.00%), Space: 39.3 MB (97.37%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,28 @@
 class Solution {
     public int mySqrt(int x) {
-        int root ;
-        root = (int) Math.sqrt (x);
-        return root;
+        long start =0;
+        long end = x/2;
+        long ans = 0;
+
+        if(x == 0 || x == 1)
+            return x;
+        while(start <= end)
+        {
+           long  mid = (start + end) / 2;
+            if(mid * mid == x)
+            {
+                return (int)mid;
+            }
+            if(mid * mid < x)
+            {
+                start = mid + 1;
+                ans = mid;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+        }
+        return (int)ans;
     }
 }
