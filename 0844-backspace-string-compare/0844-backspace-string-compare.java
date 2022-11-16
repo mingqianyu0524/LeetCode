8bf996fb3a10a5e4f0510cbe531eb734efa6e557
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-16 21:04:59 +0800

    Time: 4 ms (49.92%), Space: 42.6 MB (17.64%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+    private String build(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c != '#') {
+                stack.push(c);
+            } else {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }
+        }
+        return String.valueOf(stack);
+    }
+}
