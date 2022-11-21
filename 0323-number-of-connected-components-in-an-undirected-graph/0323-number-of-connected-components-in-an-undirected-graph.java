e586cc1a5619079ed4113c70e452b68b9dba33d4
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-21 15:58:53 +0800

    Time: 7 ms (55.70%), Space: 48.4 MB (26.97%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,12 +1,12 @@
 class Solution {
     public int countComponents(int n, int[][] edges) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
+        List<Integer>[] graph = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            graph.put(i, new ArrayList<>());
+            graph[i] = new ArrayList<Integer>();
         }
         for (int[] edge : edges) {
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
         }
         boolean[] visited = new boolean[n];
         int count = 0;
@@ -18,9 +18,9 @@ class Solution {
         }
         return count;
     }
-    private void dfs(int v, boolean[] visited, Map<Integer, List<Integer>> adj) {
+    private void dfs(int v, boolean[] visited, List<Integer>[] adj) {
         visited[v] = true;
-        for (int w : adj.get(v)) {
+        for (int w : adj[v]) {
             if (!visited[w]) dfs(w, visited, adj);
         }
         return;
