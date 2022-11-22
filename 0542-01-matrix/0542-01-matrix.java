9536ed9ae1ecc9a0a80fdc0b912b049bcbb499d4
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-22 13:41:24 +0800

    Time: 13 ms (88.79%), Space: 44.1 MB (98.00%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -3,38 +3,27 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
         int[][] dirs = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
-        
         Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[m][n];
-        
         // add 0 cells as starting points into the queue
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    queue.add(new int[]{i, j});
-                    visited[i][j] = true;
-                }
+                if (mat[i][j] == 0) queue.add(new int[]{i, j});
+                else mat[i][j] = -1;
             }
         }
         
-        // bfs
+        // BFS
         while (!queue.isEmpty()) {
             int[] cell = queue.poll();
             int r = cell[0], c = cell[1];
             for (int[] dir : dirs) {
                 int nr = r + dir[0], nc = c + dir[1];
-                if (isValid(mat, nr, nc) && !visited[nr][nc]) {
-                    visited[nr][nc] = true;
-                    queue.add(new int[]{nr, nc});
-                    mat[nr][nc] = mat[r][c] + 1; // inplace modification
-                }
+                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) 
+                    continue;
+                mat[nr][nc] = mat[r][c] + 1; // inplace modification
+                queue.add(new int[]{nr, nc});
             }
         }
         return mat;
-    }
-    
-    private boolean isValid(int[][] mat, int i, int j) {
-        int m = mat.length, n = mat[0].length;
-        return i >= 0 && i < m && j >= 0 && j < n && mat[i][j] == 1;
     }
 }
