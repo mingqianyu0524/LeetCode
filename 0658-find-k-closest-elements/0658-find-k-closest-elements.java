aa75c109e1909fed5055d20f77ff6b8b0c66fbd2
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-22 21:40:15 +0800

    Time: 12 ms (63.00%), Space: 62.8 MB (31.51%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,24 +1,18 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] nums, int k, int x) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>(
-            (n1, n2) -> {
-                if (Math.abs(n1 - x) == Math.abs(n2 - x)) {
-                    return n2 - n1;
-                }
-                return Math.abs(n2 - x) - Math.abs(n1 - x);
-            }
-        );
-        for (int num : nums) {
-            heap.add(num);
-            if (heap.size() > k) {
-                heap.remove();
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int lo = 0;
+		int hi = arr.length - 1;
+        while (hi - lo >= k) {
+            if (Math.abs(arr[lo] - x) <= Math.abs(arr[hi] - x)) {
+                hi--;
+            } else {
+                lo++;
             }
         }
-        List<Integer> ans = new LinkedList<>();
-        for (int num : heap) {
-            ans.add(num);
+        List<Integer> res = new LinkedList<>();
+        for (int i = lo; i <= hi; i++) {
+            res.add(arr[i]);
         }
-        Collections.sort(ans);
-        return ans;
+        return res;
     }
 }
