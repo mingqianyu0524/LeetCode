390ba71c18785d0ae5da3a55ff88c9648ac75c49
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-22 13:25:44 +0800

    Time: 46 ms (18.81%), Space: 73.1 MB (37.51%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,26 +1,17 @@
 class Solution {
-    class State {
-        int row; int col; int step;
-        State(int row, int col, int step) {
-            this.row = row;
-            this.col = col;
-            this.step = step;
-        }
-    }
-    
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         int[][] dirs = new int[][]{{-1,0},{1,0},{0,-1},{0,1}};
         
-        Queue<State> queue = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
         boolean[][] visited = new boolean[m][n];
         
         // add 0 cells as starting points into the queue
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
-                    queue.add(new State(i, j, 1));
+                    queue.add(new int[]{i, j});
                     visited[i][j] = true;
                 }
             }
@@ -28,14 +19,14 @@ class Solution {
         
         // bfs
         while (!queue.isEmpty()) {
-            State state = queue.poll();
-            int r = state.row, c = state.col, step = state.step;
+            int[] cell = queue.poll();
+            int r = cell[0], c = cell[1];
             for (int[] dir : dirs) {
                 int nr = r + dir[0], nc = c + dir[1];
                 if (isValid(mat, nr, nc) && !visited[nr][nc]) {
                     visited[nr][nc] = true;
-                    queue.add(new State(nr, nc, step + 1));
-                    mat[nr][nc] = step; // inplace modification
+                    queue.add(new int[]{nr, nc});
+                    mat[nr][nc] = mat[r][c] + 1; // inplace modification
                 }
             }
         }
