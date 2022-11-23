b7be8802a128f3d5400f42c0491f5c76b37d99bc
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-23 15:39:40 +0800

    Time: 1 ms (87.36%), Space: 41 MB (45.72%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public int maximum69Number (int num) {
-        String s = Integer.toString(num);
         StringBuilder sb = new StringBuilder();
-        boolean flipped = false;
-        for (char c : s.toCharArray()) {
-            if (c == '6' && !flipped) {
-                c = '9';
-                flipped = true;
+        sb.append(num);
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '6') {
+                sb.setCharAt(i, '9');
+                break;
             }
-            sb.append(c);
         }
         return Integer.parseInt(sb.toString());
     }
