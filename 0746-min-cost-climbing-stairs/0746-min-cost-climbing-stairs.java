ff8997f5ea2680d383237673c467864567fc24a6
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-27 14:52:31 +0800

    Time: 2 ms (34.54%), Space: 43.1 MB (74.84%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,14 +1,10 @@
 class Solution {
-    Map<Integer, Integer> memo;
     public int minCostClimbingStairs(int[] cost) {
-        memo = new HashMap<>();
-        return dp(cost, cost.length);
-    }
-    private int dp(int[] cost, int i) {
-        if (i <= 1) return 0;
-        if (memo.containsKey(i))
-            return memo.get(i);
-        memo.put(i, Math.min(dp(cost, i-1) + cost[i-1], dp(cost, i-2) + cost[i-2]));
-        return memo.get(i);
+        int[] dp = new int[cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= cost.length; i++)
+            dp[i] = Math.min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        return dp[cost.length];
     }
 }
