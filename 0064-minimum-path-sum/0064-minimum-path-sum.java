9f0507e8959f33eb0728f930715e6f4322bc1b93
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-28 11:54:36 +0800

    Time: 1 ms (99.96%), Space: 45.2 MB (78.95%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,24 +1,24 @@
 class Solution {
+    private int[][] memo;
+    
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[] dp = new int[n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    dp[j] = grid[i][j];
-                }
-                else if (i == 0 && j != 0) {
-                    dp[j] = grid[i][j] + dp[j-1];
-                }
-                else if (i != 0 && j == 0) {
-                    dp[j] = grid[i][j] + dp[j];
-                }
-                else {
-                    dp[j] = grid[i][j] + Math.min(dp[j], dp[j-1]);
-                }
-            }
+        int m = grid.length, n = grid[0].length;
+        memo = new int[m][n];
+        for (int[] row : memo)
+            Arrays.fill(row, -1);
+        dp(grid, m, n, 0, 0);
+        return memo[0][0];
+    }
+    
+    private int dp(int[][] M, int m, int n, int i, int j) {
+        if (i >= m || j >= n)
+            return Integer.MAX_VALUE;
+        if (i == m - 1 && j == n - 1) {
+            memo[i][j] = M[i][j];
         }
-        return dp[n-1];
-     }
+        if (memo[i][j] != -1)
+            return memo[i][j];
+        memo[i][j] = Math.min(dp(M, m, n, i+1, j), dp(M, m, n, i, j+1)) + M[i][j];
+        return memo[i][j];
+    }
 }
