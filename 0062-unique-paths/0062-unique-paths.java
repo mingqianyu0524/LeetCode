f5fb076a547a61f8b2320f7663de6325f76e2da3
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-28 11:20:21 +0800

    Time: 2 ms (31.13%), Space: 41.1 MB (28.85%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,16 +1,15 @@
 class Solution {
-    int[][] memo;
     public int uniquePaths(int m, int n) {
-        memo = new int[m][n];
-        for (int[] row : memo) 
-            Arrays.fill(row, -1);
-        return dfs(m, n, 0, 0);
-    }
-    private int dfs(int m, int n, int r, int c) {
-        if (r >= m || c >= n) return 0;
-        if (r == m-1 && c == n-1) return 1;
-        if (memo[r][c] != -1) return memo[r][c];
-        memo[r][c] = dfs(m, n, r+1, c) + dfs(m, n, r, c+1);
-        return memo[r][c];
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++)
+            dp[i][0] = 1;
+        for (int j = 0; j < n; j++)
+            dp[0][j] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
