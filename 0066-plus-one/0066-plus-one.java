75c97b4a6eca8e408c0ce94de84e00cfb0669453
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-11-30 15:53:51 +0800

    Time: 1 ms (26.23%), Space: 42.8 MB (22.49%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            int digit = digits[i];
+            digits[i] = (digits[i] + carry) % 10;
+            carry = (digit + carry) / 10;
+        }
+        if (digits[0] == 0) {
+            digits = new int[digits.length + 1];
+            digits[0] = 1;
+        }
+        return digits;
+    }
+}
