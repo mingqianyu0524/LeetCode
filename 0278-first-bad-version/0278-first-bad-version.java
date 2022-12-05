babe71005eb62ab3a4be5ed197dadf5f409ba614
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-12-05 22:11:16 +0800

    Time: 19 ms (81.89%), Space: 39 MB (89.12%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -3,14 +3,15 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int l = 1, r = n;
-        while (l < r) {
-            int mid = l + (r-l)/2;
-            if (!isBadVersion(mid))
-                l = mid + 1;
-            else
-                r = mid;
-        }
-        return l;
+        return binarySearch(1, n);
+        
+    }
+    private int binarySearch(int l, int r) {
+        if (l >= r) return l;
+        int mid = l + (r-l)/2;   
+        if (!isBadVersion(mid))
+            return binarySearch(mid+1, r);
+        else
+            return binarySearch(l, mid);
     }
 }
