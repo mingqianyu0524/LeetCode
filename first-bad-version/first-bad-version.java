d2d47ffd29fef0b94da8d30c64890a71ae2ddea4
Mingqian Yu <49342802+mingqianyu0524@users.noreply.github.com>
2022-12-05 22:11:45 +0800

    Time: 34 ms, Memory: 41 MB - LeetHub

---
+++ first-bad-version/first-bad-version.java
@@ -0,0 +1,16 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        return binarySearch(1, n);
+    }
+    private int binarySearch(int l, int r) {
+        if (l >= r) return l;
+        int mid = l + (r-l)/2;   
+        if (!isBadVersion(mid))
+            return binarySearch(mid+1, r);
+        else
+            return binarySearch(l, mid);
+    }
+}
